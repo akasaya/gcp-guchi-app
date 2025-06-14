700ed91aadf0cfccb14c0fbcb99d5118792b6d76
akasaya <zantetebak@gmail.com>
2025-06-14 18:55:04 +0900

    タグの名前、要約のプロンプト修正

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -26,7 +26,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: '愚痴アプリ MVP',
+      title: 'マインドソート',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
