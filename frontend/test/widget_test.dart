3fa2c683d7b27fa6d42857f01e5c2617bde763a2
akasaya <zantetebak@gmail.com>
2025-06-20 03:03:59 +0900

    flutter analyzeへの修正

--- frontend/test/widget_test.dart
+++ frontend/test/widget_test.dart
@@ -1,7 +1,6 @@
 import 'package:flutter_test/flutter_test.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_auth_mocks/firebase_auth_mocks.dart';
 import 'package:firebase_core_platform_interface/firebase_core_platform_interface.dart';
 
