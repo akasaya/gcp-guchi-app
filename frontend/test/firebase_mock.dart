966c162407f26a54dc5ebba458727e5bb936c7ff
akasaya <zantetebak@gmail.com>
2025-06-29 09:42:26 +0900

    firebase_mock.dartの修正

--- frontend/test/firebase_mock.dart
+++ frontend/test/firebase_mock.dart
@@ -1,4 +1,4 @@
-import 'package:firebase_auth/firebase_auth.dart';
+//import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core_platform_interface/firebase_core_platform_interface.dart';
 import 'package:flutter_test/flutter_test.dart';
 // import 'package:mockito/mockito.dart'; // 不要なため削除
